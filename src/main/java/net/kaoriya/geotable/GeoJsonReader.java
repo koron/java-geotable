3295d6f58e19f6d464a0bbd734983999c92938d6
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-11-15 18:41:28 +0900

    read geojson and coverted it S2CellUnion

--- src/main/java/net/kaoriya/geotable/GeoJsonReader.java
+++ src/main/java/net/kaoriya/geotable/GeoJsonReader.java
@@ -1,37 +1,95 @@
 package net.kaoriya.geotable;
 
-import java.io.BufferedReader;
-import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Arrays;
-import javax.json.Json;
-import javax.json.JsonReader;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiPredicate;
 
-public class GeoJsonReader implements Closeable {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.geometry.S2CellUnion;
+import com.google.common.geometry.S2LatLng;
+import com.google.common.geometry.S2Loop;
+import com.google.common.geometry.S2Point;
+import com.google.common.geometry.S2PolygonBuilder;
+import com.google.common.geometry.S2RegionCoverer;
+import org.geojson.Feature;
+import org.geojson.FeatureCollection;
+import org.geojson.GeoJsonObject;
+import org.geojson.LngLatAlt;
+import org.geojson.MultiPolygon;
+import org.geojson.Polygon;
 
-    public static class Item {
-        public String value;
+public class GeoJsonReader {
+
+    final S2RegionCoverer coverer;
+
+    public GeoJsonReader() {
+        this(new S2RegionCoverer());
     }
 
-    final JsonReader jr;
-    final ArrayList<String> props = new ArrayList<>();
-
-    public GeoJsonReader(File file, String... props) throws IOException {
-        jr = Json.createReader(Files.newBufferedReader(file.toPath(),
-                    StandardCharsets.UTF_8));
-        this.props.addAll(Arrays.asList(props));
+    public GeoJsonReader(S2RegionCoverer coverer) {
+        this.coverer = coverer;
     }
 
-    public Item read() throws IOException {
-        // TODO:
+    public void readAll(File file, BiPredicate<S2CellUnion, Map<String, Object>> fn) throws IOException {
+        FeatureCollection fc = read(file);
+        for (Feature f : fc) {
+            S2CellUnion cu = toCellUnion(f);
+            if (!fn.test(null, f.getProperties())) {
+                break;
+            }
+        }
+    }
+
+    S2CellUnion toCellUnion(Feature f) {
+        GeoJsonObject o = f.getGeometry();
+        if (o instanceof MultiPolygon) {
+            return toCellUnion((MultiPolygon)o);
+        } else if (o instanceof Polygon) {
+            return toCellUnion((Polygon)o);
+        }
         return null;
     }
 
-    public void close() throws IOException {
-        jr.close();
+    S2CellUnion toCellUnion(MultiPolygon p) {
+        S2PolygonBuilder b = new S2PolygonBuilder();
+        for (List<List<LngLatAlt>> coords : p.getCoordinates()) {
+            appendAsPolygon(b, coords);
+        }
+        return coverer.getCovering(b.assemblePolygon());
     }
+
+    S2CellUnion toCellUnion(Polygon p) {
+        S2PolygonBuilder b = new S2PolygonBuilder();
+        appendAsPolygon(b, p.getCoordinates());
+        return coverer.getCovering(b.assemblePolygon());
+    }
+
+    S2Point toPoint(LngLatAlt p) {
+        double latitude = p.getLatitude();
+        double longitude = p.getLongitude();
+        return S2LatLng.fromDegrees(latitude, longitude).toPoint();
+    }
+
+    S2Loop toLoop(List<LngLatAlt> list) {
+        int size = list.size() - 1;
+        ArrayList<S2Point> points = new ArrayList<>(size);
+        for (int i = 0; i < size; ++i) {
+            points.add(toPoint(list.get(i)));
+        }
+        return new S2Loop(points);
+    }
+
+    void appendAsPolygon(S2PolygonBuilder b, List<List<LngLatAlt>> coords) {
+        for (List<LngLatAlt> item : coords) {
+            b.addLoop(toLoop(item));
+        }
+    }
+
+    public static FeatureCollection read(File file) throws IOException {
+        return new ObjectMapper().readValue(file, FeatureCollection.class);
+    }
+
 }
---
+++ src/main/java/net/kaoriya/geotable/cmd/LoadGeoJson.java
@@ -0,0 +1,24 @@
+package net.kaoriya.geotable.cmd;
+
+import java.io.File;
+
+import org.geojson.FeatureCollection;
+
+import net.kaoriya.geotable.GeoJsonReader;
+
+public class LoadGeoJson {
+    public static void main(String[] args) throws Exception {
+        long st = System.nanoTime();
+        File f = new File("./tmp/tokyo2-format.geojson");
+        //File f = new File("../japan-prefecture-quering/tmp/japan_cities.geojson");
+        GeoJsonReader r = new GeoJsonReader();
+        final int[] size = new int[]{0};
+        r.readAll(f, (cu, props) -> {
+            ++size[0];
+            return true;
+        });
+        System.out.println("size=" + size[0]);
+        long d = System.nanoTime() - st;
+        System.out.println(String.format("%.6f", (double)d / 1000000000));
+    }
+}
